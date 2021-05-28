675fc710c51c59436e892d44f20417a314f35530
ghe <lili@snyk.io>
2021-05-28 10:54:29 +0100

    test: test conversion e2e

--- test/system/cmds/snyk:test.spec.ts
+++ test/system/cmds/snyk:test.spec.ts
@@ -19,6 +19,51 @@ describe('`snyk2spdx snyk:test`', () => {
       done();
     });
   }, 70000);
+  it('`snyk:test` output converted to SPDX for a project with no dependencies', (done) => {
+    const cliOutput = path.resolve(
+      __dirname,
+      '../../',
+      'fixtures',
+      'no-deps.json',
+    );
+    exec(`cat ${cliOutput} | node ${main}`, (err, stdout, stderr) => {
+      expect(err).toBeNull();
+      expect(stderr).toEqual('');
+      expect(JSON.parse(stdout)).toMatchObject({
+        id: 'SPDXRef-no-prod-deps',
+        name: 'no-prod-deps',
+        specVersion: 'SPDX-3.0',
+        profile: ['base', 'vulnerabilities'],
+        dataLicense: 'CC0-1.0',
+        creator: 'Organization: Snyk Ltd',
+        vulnerabilities: [],
+      });
+      done();
+    });
+  }, 70000);
+
+  it('`snyk:test` output converted to SPDX for a projects with vulnerabilities', (done) => {
+    const cliOutput = path.resolve(
+      __dirname,
+      '../../',
+      'fixtures',
+      'no-deps.json',
+    );
+    exec(`cat ${cliOutput} | node ${main}`, (err, stdout, stderr) => {
+      expect(err).toBeNull();
+      expect(stderr).toEqual('');
+      expect(JSON.parse(stdout)).toMatchObject({
+        id: 'SPDXRef-no-prod-deps',
+        name: 'no-prod-deps',
+        specVersion: 'SPDX-3.0',
+        profile: ['base', 'vulnerabilities'],
+        dataLicense: 'CC0-1.0',
+        creator: 'Organization: Snyk Ltd',
+        vulnerabilities: [],
+      });
+      done();
+    });
+  }, 70000);
   it('Shows help text as expected when calling command explicitly', (done) => {
     exec(`node ${main} snyk:jest help`, (err, stdout) => {
       if (err) {
